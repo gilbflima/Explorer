71307d6fa7e5b123076e26656e79b0fda431453f
GilbertoLima <gilbflima@gmail.com>
2023-03-28 21:55:48 -0300

    Guess the number application finished. Refactor in the next commit

--- Project 10 - Guess the number/main.js
+++ Project 10 - Guess the number/main.js
@@ -1,8 +1,31 @@
-const numberMatch = Math.ceil(Math.random()*10)
-let attempts = 0
-function handleClick(event){
-  event.preventDefault()
+//Variables
+const buttonTry = document.querySelector("#buttonTry")
+const buttonAgain = document.querySelector("#buttonAgain")
+let numberMatch = Math.ceil(Math.random()*10)//Application (choosing the number)
+let attempts = 1
+//callback functions
+function handleTryClick(event){
+  event.preventDefault()//prevent the default - send the formulary
+
   const inputNumber = document.querySelector("#inputNumber")
+  
+  if(Number(inputNumber.value) == numberMatch){
+    document.querySelector(".screen1").classList.add("show")
+    document.querySelector(".screen2").classList.remove("show")
+
+    document
+      .querySelector(".screen2 h2")
+      .innerText = (`You guessed right with ${attempts} chances.`)
+  }
+  inputNumber.value = ""
   attempts++
-  console.log(attempts)
 }
+function handleAgainClick(){
+  document.querySelector(".screen2").classList.add("show")
+  document.querySelector(".screen1").classList.remove("show")
+  attempts = 1
+  numberMatch = Math.ceil(Math.random()*10)
+}
+//EventListener
+buttonTry.addEventListener('click', handleTryClick)
+buttonAgain.addEventListener('click', handleAgainClick)
