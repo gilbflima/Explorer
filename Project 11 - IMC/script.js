db987b4812e855900a24f347bbfbcdda85242ce7
GilbertoLima <gilbflima@gmail.com>
2023-05-30 13:48:44 -0300

    Project finished

--- Project 11 - IMC/script.js
+++ Project 11 - IMC/script.js
@@ -3,9 +3,23 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
+/*const modalWrapper = document.querySelector('.modal-wrapper')
 const modalMessage = document.querySelector('.modal .title span')
-const modalButtonClose = document.querySelector('.modal button.close')
+const modalButtonClose = document.querySelector('.modal button.close')*/
+
+const Modal = {
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
+
+  visible() {
+    Modal.wrapper.classList.add('visible')
+  },
+  invisible() {
+    Modal.wrapper.classList.remove('visible')
+  }
+}
+ 
 
 form.onsubmit = event => {
   event.preventDefault()
@@ -16,12 +30,12 @@ form.onsubmit = event => {
   const result = IMC (weight, height)
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerText = message
-  modalWrapper.classList.add('visible')
+  Modal.message.innerText = message
+  Modal.visible()
 }
 
-modalButtonClose.onClick = () => {
-  modalWrapper.classList.remove('visible')
+Modal.buttonClose.onclick = () => {
+  Modal.invisible()
 }
 function IMC (weight, height) {
   return (weight/((height/100)**2)).toFixed(2)
